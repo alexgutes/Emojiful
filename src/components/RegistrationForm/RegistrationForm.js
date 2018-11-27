3504c4f8c3c5c04b4e23e73fa892f6a8ac60b8e8
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-11-27 08:16:06 -0700

    input

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Field, reduxForm, focus } from 'redux-form';
 import { registerUser } from '../../actions/users';
 import { login } from '../../actions/auth';
-import Input from '../Input';
+import Input from '../input';
 import {
   required, nonEmpty, matches, length, isTrimmed,
 } from '../../validators';
