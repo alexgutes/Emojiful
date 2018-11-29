411e6c6ed2ca107885d9a004868e39f1202f6d0c
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-11-29 11:05:31 -0700

    afternoon demo

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { Field, reduxForm, focus } from "redux-form";
-import Input from "../input";
+import Input from "../Input";
 import { login } from "../../actions/auth";
 import { required, nonEmpty } from "../../validators";
 
