afd80bd7d52022eb88d370b18f7441095990f783
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-11-29 22:35:06 -0700

    test

--- src/components/OnboardInfo.js
+++ src/components/OnboardInfo.js
@@ -1,22 +1,21 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import Emoji from './Emoji';
+import React from "react";
+import Emoji from "./Emoji";
 
 export default function OnboardInfo() {
   return (
     <div className="info one-half column">
       <h3>
-        Welcome to Emojiful
-        {' '}
-        <Emoji symbol="👋" />
+        Welcome to Emojiful <Emoji symbol="👋" />
       </h3>
       <p>Emojiful is a spaced repetition app for learing emoji quickly</p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem justo, suscipit eu massa
-        quis, aliquet hendrerit sapien. Vivamus in tristique lorem. Suspendisse id est lacus. Fusce
-        nisl ligula, volutpat a vestibulum nec, tempus eget tellus.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem
+        justo, suscipit eu massa quis, aliquet hendrerit sapien. Vivamus in
+        tristique lorem. Suspendisse id est lacus. Fusce nisl ligula, volutpat a
+        vestibulum nec, tempus eget tellus.
       </p>
-      <p>Please login or register to get started.. </p>
+      <p>Please login or register to get started... </p>
       <Emoji symbol="💯" />
     </div>
   );
